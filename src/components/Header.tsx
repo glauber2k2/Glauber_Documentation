ad671b50fd49b6d066f216073f744e6f958e9113
DevGlauber <devglaubermonteiro@gmail.com>
2024-03-15 17:23:08 -0300

    alterado logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import { FunctionComponent } from 'react'
 import ToggleLang from './ToggleLang'
+import { Code2Icon } from 'lucide-react'
 
 interface HeaderProps {}
 
 const Header: FunctionComponent<HeaderProps> = () => {
   return (
     <div className="flex items-center gap-2 bg-zinc-900 px-12 py-4 justify-between">
-      <span className="text-3xl font-bold">DevGlauber</span>
+      <span className="text-3xl font-semibold flex items-center gap-4 ">
+        <Code2Icon size={42} />
+        DevGlauber
+      </span>
       <ToggleLang />
     </div>
   )
