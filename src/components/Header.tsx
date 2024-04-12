69dd472f012672ee707de793af78778900f091b1
DevGlauber <devglaubermonteiro@gmail.com>
2024-04-12 17:04:48 -0300

    adicionado logo no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent } from 'react'
 import ToggleLang from './ToggleLang'
-import { LuCode2 } from 'react-icons/lu'
 import { Link } from '../../navigation'
 
 interface HeaderProps {}
@@ -10,7 +9,7 @@ const Header: FunctionComponent<HeaderProps> = () => {
     <div className="flex items-center py-6 px-10 justify-between border-b border-neutral-300 dark:border-neutral-800">
       <Link href={'/'}>
         <span className="text-xl font-medium flex items-center gap-4 ">
-          <LuCode2 size={32} />
+          <img src="/mylogo.png" className="object-scale-down w-10 h-10" />
           <h1 className="hidden sm:block">DevGlauber</h1>
         </span>
       </Link>
