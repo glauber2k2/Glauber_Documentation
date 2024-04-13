c28617c33ab70d80e3d11f468db05edb407d814b
DevGlauber <devglaubermonteiro@gmail.com>
2024-04-13 01:44:11 -0300

    corrigido lint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,10 @@ const Header: FunctionComponent<HeaderProps> = () => {
     <div className="flex items-center py-6 px-10 justify-between border-b border-neutral-300 dark:border-neutral-800">
       <Link href={'/'}>
         <span className="text-xl font-medium flex items-center gap-4 ">
-          <img src="/mylogo.png" className="invert dark:invert-0 object-scale-down w-10 h-10" />
+          <img
+            src="/mylogo.png"
+            className="invert dark:invert-0 object-scale-down w-10 h-10"
+          />
           <h1 className="hidden sm:block">DevGlauber</h1>
         </span>
       </Link>
