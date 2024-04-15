d7dd401f06a0d9a9bd67f2e803669445e56b6569
DevGlauber <devglaubermonteiro@gmail.com>
2024-04-14 21:10:19 -0300

    aumentado font title home mobile

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           <LuArrowRight />
         </Button>
       </IntlLink>
-      <h1 className="text-xl sm:text-3xl lg:text-6xl font-bold">
+      <h1 className="text-2xl sm:text-3xl lg:text-6xl font-bold">
         {home('title')}
       </h1>
       <div>
