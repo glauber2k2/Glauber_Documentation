8bc522699740eedcf23a9009e5fe81ecfe0f9b25
DevGlauber <devglaubermonteiro@gmail.com>
2025-06-03 15:58:28 -0300

    melhorado portfolio

--- src/app/[locale]/(sessions)/about/page.tsx
+++ src/app/[locale]/(sessions)/about/page.tsx
@@ -1,29 +1,29 @@
-import { Badge } from '@/components/ui/badge'
-import { Progress } from '@/components/ui/progress'
-import { useTranslations } from 'next-intl'
-import { FunctionComponent } from 'react'
-import { LuCalendar } from 'react-icons/lu'
+import { Badge } from "@/components/ui/badge";
+import { Progress } from "@/components/ui/progress";
+import { useTranslations } from "next-intl";
+import { FunctionComponent } from "react";
+import { LuCalendar } from "react-icons/lu";
 
 interface AboutProps {}
 
 const About: FunctionComponent<AboutProps> = () => {
-  const about = useTranslations('about')
+  const about = useTranslations("about");
 
   return (
     <div className="flex flex-col gap-8">
       <h1 className="pb-4 border-b text-xl border-neutral-300 dark:border-neutral-800 font-medium">
-        {about('title')}
+        {about("title")}
       </h1>
 
       <div>
-        <span className=" font-medium">🙋‍♂️{about('myname')}</span>
+        <span className=" font-medium">🙋‍♂️{about("myname")}</span>
         <article className="mt-4 text-xs sm:text-sm text-justify">
-          {about('presentation')}
+          {about("presentation")}
         </article>
       </div>
 
       <div className="mt-4 space-y-4">
-        <span className=" font-medium">{about('educationTitle')}</span>
+        <span className=" font-medium">{about("educationTitle")}</span>
         <div className="flex flex-col pl-2 border-l-4">
           <h3 className="font-medium text-sm">Computer Science</h3>
           <small className=" text-xs">
@@ -34,34 +34,46 @@ const About: FunctionComponent<AboutProps> = () => {
             2020 - 2024
           </small>
         </div>
+        <div className="flex flex-col pl-2 border-l-4">
+          <h3 className="font-medium text-sm">
+            Pós Desenvolvimento Web Full-stack
+          </h3>
+          <small className=" text-xs">
+            UNIPÊ - Centro Universitário de João Pessoa
+          </small>
+          <small className="mt-2 flex items-center gap-2">
+            <LuCalendar />
+            2025 - 2026
+          </small>
+        </div>
 
         <article className="text-xs sm:text-sm text-justify">
-          {about('educationText')}
+          {about("educationText")}
         </article>
       </div>
 
       <div className="mt-4">
-        <span className=" font-medium">{about('languagesTitle')}</span>
+        <span className=" font-medium">{about("languagesTitle")}</span>
         <div className="space-y-4 mt-4">
           <div className="font-medium text-sm">
             <span className="flex items-center gap-2">
-              {about('portuguese')}
+              {about("portuguese")}
               <Badge className="text-xs">Nativo</Badge>
             </span>
             <Progress className="w-full md:w-1/2 h-2 mt-2" value={100} />
           </div>
           <div className="font-medium text-sm">
-            {about('english')}
+            {about("english")}
             <Progress className="w-full md:w-1/2 h-2 mt-2" value={50} />
           </div>
           <div className="font-medium text-sm">
-            {about('spanish')}
+            {about("spanish")}
             <Progress className="w-full md:w-1/2 h-2 mt-2" value={80} />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
